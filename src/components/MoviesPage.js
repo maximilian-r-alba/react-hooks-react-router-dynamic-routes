936ddb206a09f49c3f8777b0077337708192097d
Maximilian Alba <albam476@gmail.com>
2022-08-28 13:04:47 -0400

    commit tree 592ffccfd0edf63f0de58188d3fe24bc1fafd95b

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,26 @@
+// .src/components/MoviesPage.js
 import React from "react";
-import { Route } from "react-router-dom";
+// import the custom `useRouteMatch` hook from React Router
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+// import the MovieShow component
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
-  return (
+  const match = useRouteMatch();
+  console.log(match);
+
+    return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
+
 }
 export default MoviesPage;
